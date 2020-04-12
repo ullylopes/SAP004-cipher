21c8500513837911c476bda3aa9a5d04d2296000
Laboratoria <you@example.com>
2020-04-12 19:40:16 -0300

    Primeiro Commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,49 @@
 const cipher = {
-  // ...
-};
+
+    encode: function cipher(offset, message) {
+        let displacement = Number(offset);
+        let textArr = [];
+
+        if ((message !== 0 && message !== null) && (offset !== 0 && offset !== null)) {
+            for (let count = 0; count < message.length; count++) {
+                let ascii = message.toUpperCase().charCodeAt(count);
+
+                if (ascii !== 32) {
+                    let formulaNewLetter = String.fromCharCode(((ascii - 65 + displacement) % 26) + 65);
+                    textArr.push(formulaNewLetter);
+                } else {
+                    textArr.push(" ");
+                }
+            }
+        } else {
+            throw new TypeError;
+        }
+
+        return textArr.join("");
+    },
+
+
+    decode: function decipher(offset, message) {
+        let displacement = Number(offset);
+        let textArr = [];
+        if ((message !== 0 && message !== null) && (offset !== 0 && offset !== null)) {
+            for (let count = 0; count < message.length; count++) {
+                let ascii = message.toUpperCase().charCodeAt(count);
+
+                if (ascii !== 32) {
+                    let formulaNewLetter = String.fromCharCode(((ascii + 65 - displacement) % 26) + 65);
+                    textArr.push(formulaNewLetter);
+                } else {
+                    textArr.push(" ");
+                }
+            }
+        } else {
+            throw new TypeError;
+        }
+
+        return textArr.join("");
+    }
+
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import cipher from './cipher.js';
 
+let cipherButton = document.querySelector("input#cipher");
+let decipherButton = document.querySelector("input#decipher");
+
+
+function printCipher() {
+    let message = document.querySelector("textarea#msg").value.toUpperCase();
+    let offset = document.querySelector("input#offset").value;
+    if (message === "" || offset === "") {
+        alert("Preencha o campo da mensagem e o valor do deslocamento!");
+    } else {
+        document.querySelector("div#res").innerHTML = cipher.encode(offset, message);
+    }
+}
+function printDecipher() {
+    let message = document.querySelector("textarea#msg").value.toUpperCase();
+    let offset = document.querySelector("input#offset").value;
+    if (message === "" || offset === "") {
+
+        alert("Preencha o campo da mensagem e o valor do deslocamento!");
+    } else {
+        document.querySelector("div#res").innerHTML = cipher.decode(offset, message);
+
+    }
+}
+
+cipherButton.addEventListener("click", printCipher);
+decipherButton.addEventListener("click", printDecipher);
+
+
 console.log(cipher);
